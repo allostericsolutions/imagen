09f2e529473cfb1c145b0cbae343232f2d23823c
allostericsolutions <ciclosporina2@hotmail.com>
2024-06-25 21:12:16 -0400

    Add files via upload

---
+++ imagen.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Ruta de la imagen en GitHub 
+image_url = "https://github.com/allostericsolutions/imagen/blob/main/Allosteric_Solutions.png" 
+
+st.title("Mi primera aplicación Streamlit")
+
+st.image(image_url, width=300)
